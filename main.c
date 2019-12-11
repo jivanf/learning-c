5fb0c4f16f1fca015891ace3b432624e8cdbe6ec
Jivan <minecraftjivan@gmail.com>
2019-12-10 18:32:32 -0800

    If-else conditions

--- main.c
+++ main.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 
-#define BEGIN 300
-#define END 0
+#define BEGIN 0
+#define END 420
 #define STEP 20
 
 int main() {
 	float celsius;
 	int fahr;
 	
-		printf("FAHRENHEIT TO CELSIUS TABLE\n");
-		printf("┌────────────┬─────────┐\n");
-		printf("│ FAHRENHEIT │ CELSIUS │\n");
-		printf("├────────────┼─────────┤\n");
+	printf("FAHRENHEIT TO CELSIUS TABLE\n");
+	printf("┌────────────┬─────────┐\n");
+	printf("│ FAHRENHEIT │ CELSIUS │\n");
+	printf("├────────────┼─────────┤\n");
 
-	for (fahr = BEGIN; fahr >= END; fahr = fahr - STEP) { 
+	for (fahr = BEGIN; fahr <= END; fahr = fahr + STEP) { 
 		celsius = 5.0/9.0 * (fahr - 32.0);
 
 		printf("│    %3d     │  %5.1f  │\n", fahr, celsius);
-		printf("├────────────┼─────────┤\n");
-		printf("│            │         │\n");
+
+		if (fahr + STEP > END) {
+			printf("└────────────┴─────────┘\n");
+		}
+		else {
+			printf("├────────────┼─────────┤\n");
+			printf("│            │         │\n");
+		}
 	}
 
 }
