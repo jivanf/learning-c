cc3962b88f9ca3cc7c4aa53ae75902fac2d4884d
Jivan <minecraftjivan@gmail.com>
2019-12-10 17:56:36 -0800

    Variables, while loops and arithmetic

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello world!\n");
+	float celsius;
+	int fahr, begin, end, step;
+
+	begin = 0;
+	end = 300;
+	step = 20;
+	
+	fahr = begin;
+	printf("FAHRENHEIT TO CELSIUS TABLE\n");
+	printf("┌────────────┬─────────┐\n");
+	printf("│ FAHRENHEIT │ CELSIUS │\n");
+	printf("├────────────┼─────────┤\n");
+	while (fahr <= end) {
+		celsius = 5.0/9.0 * (fahr - 32.0);
+
+		printf("│ %3d        │%6.1f   │\n", fahr, celsius);
+		printf("├────────────┼─────────┤\n");
+		printf("│            │         │\n");
+
+		fahr = fahr + step;
+	}
+
 }
