a35be63dcc3b81ba1245ed4864cdcd76bf546470
Jivan <minecraftjivan@gmail.com>
2019-12-10 20:38:45 -0800

    Input and output

--- main.c
+++ main.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
 
-#define BEGIN 0
-#define END 420
-#define STEP 20
-
 int main() {
-	float celsius;
-	int fahr;
-	
-	printf("FAHRENHEIT TO CELSIUS TABLE\n");
-	printf("┌────────────┬─────────┐\n");
-	printf("│ FAHRENHEIT │ CELSIUS │\n");
-	printf("├────────────┼─────────┤\n");
-
-	for (fahr = BEGIN; fahr <= END; fahr = fahr + STEP) { 
-		celsius = 5.0/9.0 * (fahr - 32.0);
-
-		printf("│    %3d     │  %5.1f  │\n", fahr, celsius);
-
-		if (fahr + STEP > END) {
-			printf("└────────────┴─────────┘\n");
+	int ch, count;
+	count = 0;
+	printf("Value of EOF is %d\n", EOF);
+	while ((ch = getchar()) != EOF) {
+		if (ch == 10) {
+			printf("Character count: %d\n", count);
+			count = 0;
 		}
+
 		else {
-			printf("├────────────┼─────────┤\n");
-			printf("│            │         │\n");
+			printf("ASCII code of character %c: %d\n", ch, ch);
+			printf("\n");
+			++count;
 		}
 	}
-
 }
