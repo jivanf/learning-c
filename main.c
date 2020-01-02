da20aadd8b3bae0ad2ebd5719a82bc15b9b32c64
Jivan <minecraftjivan@gmail.com>
2020-01-01 20:01:31 -0800

    Functions and recursion

--- main.c
+++ main.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 
+#define BASE 	 -3
+#define EXPONENT 3
+
+/* Raise number x to a power y */
+int power(int x, int y);
+
+int power(x, y) {
+	if (y == 0) {
+		return 1;
+	}
+
+	else {
+		return x * power(x, y - 1);
+	}
+}
+
 int main() {
-	int digit_count[10];
-	int c, i, white_count, other_count;
-	white_count = other_count = 0;
-	
-	for (i = 0; i <= 9; i++) {
-		digit_count[i] = 0;
-	}
-
-	while ((c = getchar()) != EOF) {
-		if (c >= '0' && c <= '9') {
-			++digit_count[c - '0'];
-		}
-
-		else if (c == '\n' || c == '\t' || c == ' ') {
-			++white_count;
-		}
-
-		else {
-			++other_count;
-		}
-	}
-
-	printf("DIGIT COUNT:\n");
-	for (i = 0; i <= 9; i++) {
-		printf("%d: %d\n", i, digit_count[i]);
-	}
-
-	printf("WHITESPACE COUNT: %d\n", white_count);
-	printf("OTHER CHARACTER COUNT: %d\n", other_count);
+	printf("%d\n", power(BASE, EXPONENT));
+	return 0;
 }
