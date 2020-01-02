bdcb51866c54345a1d3e50f3f26250b0a8230e89
Jivan <minecraftjivan@gmail.com>
2020-01-01 16:38:32 -0800

    Exercise 1-14

---
+++ exercises/1/1-14.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+	int i, j, ch, most_repeated_char;
+	int chars[93] = {};	/* non-space ASCII characters */
+	most_repeated_char = 0;
+
+	while ((ch = getchar()) != EOF) {
+		if (ch == ' ' || ch == '\t' || ch == '\n' || (!(ch > 32) && !(ch < 127))) {
+			continue;
+		}
+
+		else {
+			++chars[ch - 33];
+		}
+
+	}
+
+	for (i = 0; i < 93; i++) {
+		if (most_repeated_char == 0) {
+			most_repeated_char = chars[i];
+		}
+
+		else {
+			if (chars[i] > most_repeated_char) {
+				most_repeated_char = chars[i];
+			}
+		}
+	}
+
+	for (i = 0; i < 93; i++) {
+		printf("%c |  ", i + 33);
+		for (j = 0; j < chars[i]; j++) {
+			printf("*  ");
+		}
+		printf("\n");
+	}
+
+	printf("  +");
+
+	for (i = 0; i < most_repeated_char; i++) {
+		printf("---");
+	}
+
+	printf("\n");
+	printf("   ");
+
+	for (i = 1; i <= most_repeated_char; i++) {
+		printf("%3d", i);
+	}
+
+	printf("\n");
+}
