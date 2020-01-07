71731b81b41cde453716f929be1d1ffa837e8904
Jivan <minecraftjivan@gmail.com>
2020-01-06 22:21:58 -0800

    Delete unused variables and copy function in exercise 1-18

--- exercises/1/1-18.c
+++ exercises/1/1-18.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 #define MAXLINESIZE 1000  /* maximum amount of characters per line */
-#define MAXTEXTSIZE 3000  /* maximum amount of characters in all lines */
-#define MINPRINT    80	  /* minimum amount of characters the line needs to be printed */
 
 int get_line(char line[], int max_len);
-void copy(char origin[], char duplicate[]);
 
 /* get_line: read a line into line, return its length in i */
 int get_line(char line[], int max_size ) {
@@ -22,23 +19,12 @@ int get_line(char line[], int max_size ) {
 	return i;
 }	
 
-/* copy: copy a line from 'origin' to 'duplicate' */
-void copy(char origin[], char duplicate[]) {
-	int i = 0;
-	while ((duplicate[i] = origin[i]) != '\0') {
-		++i;
-	}
-}
-
 int main() {
-	int line_size, max_line_size, c, i;
-	line_size = max_line_size = i = 0;
+	int line_size, c, i;
+	line_size = i = 0;
 
 	char current_line[MAXLINESIZE];
-	char max_line[MAXLINESIZE];
-	char bigger_than_min_print[MAXTEXTSIZE] = {}; 
 
-	bigger_than_min_print[MAXTEXTSIZE - 1] = '\0';
 	while ((line_size = get_line(current_line, MAXLINESIZE)) != 0) {
 		if (current_line[line_size] != '\n') {
 			while ((c = getchar()) != '\n' && c != EOF) {
