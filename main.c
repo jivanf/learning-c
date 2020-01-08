b5fed0a06edea4a429657385be7670782af1f6ec
Jivan <minecraftjivan@gmail.com>
2020-01-07 22:31:30 -0800

    Variable scope

--- main.c
+++ main.c
@@ -1,48 +1,57 @@
+/* Note: In the functions, the 'extern' keyword can be omitted because the external variables are defined in the same source file */
+/* "Common practice is to place definitions of all external variables at the beginning of the source file, and then ommit all extern declarations" */
 #include <stdio.h>
 
-#define MAXSIZE 5
+#define MAXSIZE 1000
 
-int get_line(char line[], int max_len);
-void copy(char origin[], char duplicate[]);
+int get_line(void);
+void copy(void);
+
+char current_line[MAXSIZE], max_line[MAXSIZE];
+int max_line_size;
 
 /* getline: read a line into line, return its length in i */
-int get_line(char line[], int max_len) {
+int get_line(void) {
+	extern char current_line[];
 	int c, i;
 	i = 0;
 
 	while ((c = getchar()) != '\n' && c != EOF) {
-		line[i] = c;
+		current_line[i] = c;
 		++i;
 	}
 
 	if (c == '\n') {
-		line[i] = c;
+		current_line[i] = c;
 		++i;
 	}
 
-	line[i] = '\0';
+	current_line[i] = '\0';
 	return i - 1;
 }	
 
 /* copy: copy a line from 'origin' to 'duplicate' */
-void copy(char origin[], char duplicate[]) {
+void copy(void) {
 	int i = 0;
-	while ((duplicate[i] = origin[i]) != '\0') {
+	extern char max_line[], current_line[];
+	while ((max_line[i] = current_line[i]) != '\0') {
 		++i;
 	}
 }
 		
 int main() {
-	int line_size, max_line_size, i;
-	line_size = max_line_size = i = 0;
-	char current_line[MAXSIZE];
-	char max_line[MAXSIZE];
-	while ((line_size = get_line(current_line, MAXSIZE)) != 0) {
+	extern int max_line_size;
+	extern char current_line[], max_line[];
+
+	int line_size, i;
+	line_size = i = 0;
+
+	while ((line_size = get_line()) != 0) {
 		printf("%d\n", line_size);
 
 		if (line_size > max_line_size) {
 			max_line_size = line_size;
-			copy(current_line, max_line);
+			copy();
 		}
 	}
 
